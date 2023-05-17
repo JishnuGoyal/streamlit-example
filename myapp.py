c8f30250def9542a0ba2d6328b14905a4f0fdbfe
JishnuGoyal <64526117+JishnuGoyal@users.noreply.github.com>
2023-05-17 23:17:29 +0530

    to check if app deploys

--- myapp.py
+++ myapp.py
@@ -15,17 +15,17 @@ def main():
     st.title("Loan Repayment Prediction System")
     session = requests.Session()
     with st.form("my_form"):
-        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'))
-        flag_cont_mobile = st.number_input("How many mobiles?")
-        flag_emp_phone = st.radio("Does the applicant\'s employees own phone?", ('Yes', 'No'))
-        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'))
-        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'))
+        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'), key=1)
+        flag_cont_mobile = st.number_input("How many mobiles?", key=5)
+        flag_emp_phone = st.radio("Does the applicant\'s employees own phone?", ('Yes', 'No'), key=2)
+        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'), key=3)
+        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'), key=4)
 
         submitted = st.form_submit_button("Submit")
 
         if submitted:
             st.write("Result")
-            data = fetch(session, f"https://picsum.photos/id/{index}/info")
+            data = fetch(session, f"http://192.168.43.8:3000/predict?params=0 1 1 1 1 0.052631579 0 0 0.04 0 0 0.005289629 0.005289629 0 0 0.213284583 0.005747126 0.005813953 0.108866442 0 1")
             if data:
                 st.write(data['download_url'], caption=f"The user will be able to repay: {data['author']}")
             else:
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,5 @@
-import requests
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import requests
 
 
 def fetch(session, url):
@@ -26,17 +15,17 @@ def main():
     st.title("Loan Repayment Prediction System")
     session = requests.Session()
     with st.form("my_form"):
-        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'))
-        flag_cont_mobile = st.number_input("How many mobiles?")
-        flag_emp_phone = st.radio("Does the applicant\'s employees own phone?", ('Yes', 'No'))
-        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'))
-        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'))
+        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'), key=1)
+        flag_cont_mobile = st.number_input("How many mobiles?", key=5)
+        flag_emp_phone = st.radio("Does the applicant\'s employees own phone?", ('Yes', 'No'), key=2)
+        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'), key=3)
+        flag_mobil = st.radio("Does the applicant own a mobile?", ('Yes', 'No'), key=4)
 
         submitted = st.form_submit_button("Submit")
 
         if submitted:
             st.write("Result")
-            data = fetch(session, f"https://picsum.photos/id/info")
+            data = fetch(session, f"http://192.168.43.8:3000/predict?params=0 1 1 1 1 0.052631579 0 0 0.04 0 0 0.005289629 0.005289629 0 0 0.213284583 0.005747126 0.005813953 0.108866442 0 1")
             if data:
                 st.write(data['download_url'], caption=f"The user will be able to repay: {data['author']}")
             else:
